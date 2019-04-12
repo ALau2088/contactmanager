14db5b5537e04e4e0162ae22763d14344657f18f
ALau2088 <alau12004@yahoo.com>
2019-04-12 12:56:37 -0700

    Updates

---
+++ precache-manifest.4baac3137dab8094942e412f1a4d0263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edc960af671dd7c7ab80",
+    "url": "/contactmanager/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "edc960af671dd7c7ab80",
+    "url": "/contactmanager/static/js/main.edc960af.chunk.js"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "7a103103cd498a2acc44",
+    "url": "/contactmanager/static/css/2.88f13204.chunk.css"
+  },
+  {
+    "revision": "7a103103cd498a2acc44",
+    "url": "/contactmanager/static/js/2.7a103103.chunk.js"
+  },
+  {
+    "revision": "4846fe110004092b40b4fd87c4c005ce",
+    "url": "/contactmanager/index.html"
+  }
+];
